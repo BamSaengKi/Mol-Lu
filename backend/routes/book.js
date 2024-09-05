fd52c2226967cd8ac7e3add9c172046bd3edfd54
Bam <bamsaengki@gmail.com>
2024-09-05 15:22:44 +0900

    [UPDATE]express CRUD 만드는 중

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -6,8 +6,37 @@ const pool = require("../config/DbConfig");
 // 2. 예외처리(try/catch)
 // 3. 모듈화
 
+// 도서 등록
 router.post("/", (req, res, next) => {
-    res.send(req.body);
+    const body = req.body;
+    const bookID = req.body.bookID;
+    const bookName = req.body.bookName;
+    const author = req.body.author;
+    const price = req.body.price;
+    const stock = req.body.stock;
+    const url = req.body.url;
+    pool.getConnection((err, conn) => {
+        if (err) {
+            throw err;
+        }
+        const sql = `
+
+        INSERT INTO 
+                mollu.tbBooks
+                (bookID, bookName, author, price, stock, url)
+        VALUES
+                (?, ?, ?, ?, ?, ?);
+        `;
+        conn.query(sql, [bookID, bookName, author, price, stock, url], (err, results) => {
+            if (!err) {
+                console.log(body);
+                console.log(results);
+                conn.release();
+                req.results = results;
+                res.send(results);
+            }
+        });
+    });
 });
 
 // 도서 검색
@@ -60,6 +89,85 @@ router.get(
 // 도서 상세
 router.get("/:book_id", function (req, res, next) {
     res.send(req.params);
+});
+
+// 도서 정보 변경
+router.put("/", (req, res) => {
+    const body = req.body;
+    const bookID = body.bookID;
+    const updatedData = req.body;
+
+    pool.getConnection((err, conn) => {
+        if (err) {
+            throw err;
+        }
+        console.log("db");
+
+        // 기존 도서 정보 가져오기
+        const existingSql = `
+        SELECT 
+                bookID, 
+                bookName,
+                author,
+                price,
+                stock,
+                url
+        FROM
+                tbBooks tbb
+        WHERE
+                tbb.bookID=?;
+        `;
+        conn.query(existingSql, [bookID], (err, results) => {
+            if (!err) {
+                const existingBook = results[0];
+
+                const updateedBook = {
+                    bookName: updatedData.bookName || existingBook.bookName,
+                    author: updatedData.author || existingBook.author,
+                    price: updatedData.price || existingBook.price,
+                    stock: updatedData.stock || existingBook.stock,
+                    url: updatedData.url || existingBook.url,
+                };
+
+                const sql = `
+                UPDATE
+                    mollu.tbBooks
+                SET 
+                    bookName=?, author=?, price=?, stock=?, url=?
+                WHERE 
+                    bookID=?;
+            `;
+                conn.query(
+                    sql,
+                    [
+                        updateedBook.bookName,
+                        updateedBook.author,
+                        updateedBook.price,
+                        updateedBook.stock,
+                        updateedBook.url,
+                        bookID,
+                    ],
+                    (err, results) => {
+                        if (!err) {
+                            console.log(body);
+                            console.log(results);
+                            conn.release();
+                            req.results = results;
+                            res.send(results);
+                        } else {
+                            console.log(err);
+                            return err;
+                        }
+                    }
+                );
+            } else {
+                throw err;
+            }
+        });
+    });
+
+    //수정되지 않은 필드 기존 값으로 유지
+    pool.getConnection((err, conn) => {});
 });
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,7 +2,40 @@ var express = require("express");
 var router = express.Router();
 var pool = require("../config/DbConfig");
 
-/* GET users listing. */
+// 유저 등록
+router.post("/", (req, res, next) => {
+    const body = req.body;
+    pool.getConnection((err, conn) => {
+        if (err) {
+            throw err;
+        }
+        console.log("연결은 돼따");
+        const sql = `
+    INSERT INTO
+        mollu.tbCustomer
+        (name, customerID, customerPassword)
+    VALUES
+        (?, ?, ?);
+    `;
+        const name = req.body.name;
+        const customerID = req.body.customerID;
+        const customerPassword = req.body.customerPassword;
+
+        conn.query(sql, [name, customerID, customerPassword], (err, results) => {
+            if (!err) {
+                console.log(body);
+                console.log(results);
+                conn.release();
+                res.results = results;
+            } else {
+                return err;
+            }
+        });
+    });
+    res.send(req.body);
+});
+
+// 유저 검색
 router.get(
     "/",
     function (req, res, next) {
@@ -43,5 +76,71 @@ router.get(
         res.send(req.results);
     }
 );
+
+// 유저 정보 수정
+router.put("/", (req, res) => {
+    const body = req.body;
+    const name = body.name;
+    const customerID = body.customerID;
+    const customerPassword = body.customerPassword;
+    pool.getConnection((err, conn) => {
+        if (err) {
+            throw err;
+        }
+        console.log("DB");
+        const sql = `
+          UPDATE
+              mollu.tbCustomer
+          SET
+              name=?,
+              customerPassword=?
+          WHERE 
+              customerID=?;
+        `;
+        conn.query(sql, [name, customerPassword, customerID], (err, results) => {
+            if (!err) {
+                console.log(body);
+                console.log(results);
+                conn.release();
+                req.results = results;
+                res.send(results);
+            } else {
+                console.log(err);
+                return err;
+            }
+        });
+    });
+});
+
+// 유저 삭제
+router.delete("/", (req, res) => {
+    const body = req.body;
+    pool.getConnection((err, conn) => {
+        if (err) {
+            throw err;
+        }
+        console.log("DB");
+        const sql = `
+    DELETE FROM 
+        mollu.tbCustomer
+    WHERE 
+        customerID=?;
+    `;
+        conn.query(sql, [body.customerID], (err, results) => {
+            if (!err) {
+                console.log(body);
+                console.log(results);
+                console.log(body.customerID);
+                conn.release();
+                req.results = results;
+                res.send(results);
+            } else {
+                console.log(sql);
+                console.log(err);
+                return err;
+            }
+        });
+    });
+});
 
 module.exports = router;
